935df3f8e321bc677b70d8da44cfa3558ae90f80
akinremimoses <akinremimoses93@gmail.com>
2023-10-11 20:29:55 -0700

    task 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+	/**
+	 * array_iterator - prints each array element "\n"
+	 * @array: array
+	 * @size: how many elem to print
+	 * @action: pointer to print in regular
+	 * Return: void
+	 */
+	void array_iterator(int *array, size_t size, void (*action)(int))
+	{
+		unsigned int i;
+
+
+		if (array == NULL || action == NULL)
+			return;
+
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+
