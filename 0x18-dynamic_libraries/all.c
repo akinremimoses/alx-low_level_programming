18a325bd23ed1db5bba3ddcf8c8650ec766eaeab
akinremimoses <akinremimoses93@gmail.com>
2023-12-12 06:02:50 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,46 @@
+
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub  - substract two integers
+ * @a: first int
+ * @b: second int
+ * Return: substracted final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+/**
+ * div - divide two integers
+ * @a: first int
+ * @b: second int
+ * Return: division result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - mod two integers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
